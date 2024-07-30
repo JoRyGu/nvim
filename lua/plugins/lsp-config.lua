3fa865955faedc89ce2e329d32e20fac45dc7ab4
JoRyGu <josh.gude@mentistic.com>
2024-07-29 23:44:54 -0400

    added initial lsp configs

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls", "lexical"},
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local configs = require("lspconfig.configs")
+
+      local lexical_config = {
+        filetypes = { "elixir", "eelixir", "heex" },
+        cmd = { vim.fn.stdpath("data") .. "/mason/packages/lexical/lexical" },
+        settings = {}
+      }
+
+      if not configs.lexical then
+        configs.lexical = {
+          default_config = {
+            filetypes = lexical_config.filetypes,
+            cmd = lexical_config.cmd,
+            root_dir = function(fname)
+              return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+            end,
+            settings = lexical_config.settings,
+          }
+        }
+
+      end
+
+      lspconfig.lua_ls.setup({})
+      lspconfig.lexical.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
